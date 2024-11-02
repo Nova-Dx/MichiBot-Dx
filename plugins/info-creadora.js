e9a33c2c9c89069045d5d9fd552110ad1090f092
Nova <136738258+Nova-Dx@users.noreply.github.com>
2024-11-02 01:09:59 -0500

    MichitBot

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -11,7 +11,7 @@ const cat = `MichiBot
 *---------------------*
 
 *IG*
-*https://www.instagram.com/ceogeripium.dzn*
+*https://www.instagram.com/strangehuman*
 
 Asistencia
 *${asistencia}*
@@ -31,9 +31,9 @@ let bio = biografia.status?.toString() || `${desc2 == '' ? lenguajeGB.smsContact
 
 nombre = official[0][0] == String(contact[0]) ? official[0][1] : official[1][0] == String(contact[0]) ? official[1][1] : official[2][0] == String(contact[0]) ? official[2][1] : official[3][0] == String(contact[0]) ? official[3][1] : lenguajeGB.smsContacto3() 
 description = official[0][0] == String(contact[0]) ? 'Solo temas de MichiBot' : official[1][0] == String(contact[0]) ? lenguajeGB.smsContacto4() : official[2][0] == String(contact[0]) ? lenguajeGB.smsContacto4() : official[3][0] == String(contact[0]) ? lenguajeGB.smsContacto4() : desc === '' ? lenguajeGB.smsContacto5() : desc
-correo = official[0][0] == String(contact[0]) ? 'https://www.instagram.com/ceogeripium.dzn' : official[1][0] == String(contact[0]) ? 'https://www.instagram.com/ceogeripium.dzn' : official[2][0] == String(contact[0]) ? 'https://www.instagram.com/ceogeripium.dzn' : mail === '' ? lenguajeGB.smsContacto6() : mail
+correo = official[0][0] == String(contact[0]) ? 'https://www.instagram.com/strangehuman' : official[1][0] == String(contact[0]) ? 'https://www.instagram.com/strangehuman' : official[2][0] == String(contact[0]) ? 'https://www.instagram.com/strangehuman' : mail === '' ? lenguajeGB.smsContacto6() : mail
 lugar = official[0][0] == String(contact[0]) ? 'MichiBot' : official[1][0] == String(contact[0]) ? 'MichiBot' : official[2][0] == String(contact[0]) ? 'MichiBot' : official[3][0] == String(contact[0]) ? 'MichiBot' : country === '' ? lenguajeGB.smsContacto7() : country
-enlace = official[0][0] == String(contact[0]) ? 'https://www.instagram.com/ceogeripium.dzn' : official[1][0] == String(contact[0]) ? 'https://www.instagram.com/ceogeripium.dzn' : official[2][0] == String(contact[0]) ? 'https://www.instagram.com/ceogeripium.dzn' : official[3][0] == String(contact[0]) ? 'https://www.instagram.com/ceogeripium.dzn' : md 
+enlace = official[0][0] == String(contact[0]) ? 'https://www.instagram.com/strangehuman' : official[1][0] == String(contact[0]) ? 'https://www.instagram.com/strangehuman' : official[2][0] == String(contact[0]) ? 'https://www.instagram.com/strangehuman' : official[3][0] == String(contact[0]) ? 'https://www.instagram.com/strangehuman' : md 
 
 lista.push([number, ofc, nombre, description, official[3][0] == String(contact[0]) ? null : correo, lugar, enlace, bio, official[1][0] == String(contact[0]) ? 'https://www.instagram.com/ceogeripium.dzn' : null]) }  
 lista.push([conn.user.jid.split('@')[0], await conn.getName(conn.user.jid), packname, lenguajeGB.smsContacto8(), mail === '' ? 'https://www.instagram.com/ceogeripium.dzn' : mail, lenguajeGB.smsContacto7(), md, bioBot, yt, ig, fb, paypal, nna])
