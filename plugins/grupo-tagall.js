1f19d4d5bfe5cf4d56a17c9fffb83fc885e67fcf
Nova <136738258+Nova-Dx@users.noreply.github.com>
2024-11-10 18:42:45 -0500

    MichiBot

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -5,10 +5,10 @@ throw false
 }
 let pesan = args.join` `
 let oi = `ＭｉｃｈｉＢｏｔ ${pesan}`
-let teks = `*𝘼𝙘𝙩𝙞𝙫𝙖𝙩𝙚 𝙥𝙡𝙖𝙣𝙩𝙖 𝙙𝙚 𝙢𝙧𝙙𝙖🌱🫦*\n${oi}\n\n*＠strangehuman🫶🏻*\n`
+let teks = `*🫧𝘼𝙘𝙩𝙞𝙫𝙖𝙩𝙚 𝙥𝙡𝙖𝙣𝙩𝙖 𝙙𝙚 𝙢𝙧𝙙𝙖🫧*\n${oi}\n\n*＠strangehuman🐾🤍*\n`
 for (let mem of participants) {
-teks += `🍔🍟ᝰ.ᐟ @${mem.id.split('@')[0]}\n`}
-teks += `MichiBot`
+teks += `🍕🧋ᝰ.ᐟ @${mem.id.split('@')[0]}\n`}
+teks += `𝘔𝘪𝘤𝘩𝘪𝘉𝘰𝘵`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) } )
 }
 handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
